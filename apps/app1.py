d681448040cb22e6fd9aba4ea2dd4f54ced7119f
m robillard <maxime99.robillard@gmail.com>
2020-10-20 18:45:31 +0200

    ajout des pages alcools pour les hommes et les femmes

--- apps/app1.py
+++ apps/app1.py
@@ -10,32 +10,81 @@ import dash_bootstrap_components as dbc
 active = "page1"
 nav = NavBar(active)
 
-# Figure 1 montrant le pourcentage de jeune qui consomme de l'alcool pour les 15-19 ans
+def choroplethGraph(scope,data,color,colorFrench,legendColor):
+    fig = px.choropleth(data , locations='Alpha-3 code', color=color,
+                               color_continuous_scale=legendColor,
+                               range_color=(0, 100),
+                               scope=scope,
+                               labels={color:colorFrench},
+                               template="plotly_dark",
+                              )
+    return fig
+
+# Ouverture du csv
 youth = pd.read_csv("data/youth_cont.csv")
-fig = px.choropleth(youth , locations='Alpha-3 code', color='15-19 years old, current drinkers both sexes (%)',
-                           color_continuous_scale="Peach",
-                           range_color=(0, 100),
-                           scope="world",
-                           labels={'15-19 years old, current drinkers both sexes (%)':"consommation d'alcool<br> pour les 15-19 ans"},
-                           template="plotly_dark",
-                          )
+# Data récupérant la moyenne du pourcentage d'alcool par continents
+moy = youth.groupby('Continent').mean()
 
+# Data montrant le pourcentage de jeune qui consomme de l'alcool pour les 15-19 ans
+youthColor = '15-19 years old, current drinkers both sexes (%)'
+youthColorFrench = "consommation d'alcool<br> pour les 15-19 ans"
 
-fig.update_layout(title_text = "% de jeune consommant de l'alcool"),
-fig.update_layout(margin={"r":0,"t":30,"l":0,"b":30}),
+# Data montrant le pourcentage de jeune homme qui consomme de l'alcool pour les 15-19 ans
+youthColorMan = '15-19 years old, current males drinkers (%)'
+youthColorManFrench ="consommation d'alcool<br> pour les hommes 15-19 ans"
+# Data montrant le pourcentage de jeune femme qui consomme de l'alcool pour les 15-19 ans
+youthColorFemales = '15-19 years old, current females drinkers (%)'
+youthColorFemalesFrench = "consommation d'alcool<br> pour les femmes 15-19 ans"
 
+# Figure map consommation d'alcool h+f
+figTotal = choroplethGraph("world",youth,youthColor,youthColorFrench,"Peach")
+figTotal.update_layout(title_text = "% de jeune consommant de l'alcool"),
+figTotal.update_layout(margin={"r":0,"t":30,"l":0,"b":30}),
+
+figTotal3 = px.bar(moy , y = youthColor,
+                labels={youthColor:youthColorFrench},
+                template = "plotly_dark",
+            )
+figTotal3.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
+#figure map consommation d'alcool hommes
+figTotalMan = choroplethGraph("world",youth,youthColorMan,youthColorManFrench,"Peach")
+figTotalMan.update_layout(title_text = "% de jeune hommes consommant de l'alcool"),
+figTotalMan.update_layout(margin={"r":0,"t":30,"l":0,"b":30}),
+figTotalMan3 = px.bar(moy , y = youthColorMan,
+                labels={youthColorMan:youthColorManFrench},
+                template = "plotly_dark",
+            )
+figTotalMan3.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
+#figure map consommation d'alcool femmes
+figTotalFemales = choroplethGraph("world",youth,youthColorFemales,youthColorFemalesFrench,"Peach")
+figTotalFemales.update_layout(title_text = "% de jeune femmes consommant de l'alcool"),
+figTotalFemales.update_layout(margin={"r":0,"t":30,"l":0,"b":30}),
+figTotalFemales3 = px.bar(moy , y = youthColorFemales,
+                labels={youthColorFemales:youthColorFemalesFrench},
+                template = "plotly_dark",
+            )
+figTotalFemales3.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+
+# Data pour récupérer les pays et continents
 jeunes_morts = pd.read_csv("data/jeunes_morts_cont.csv")
 pays_possibles = jeunes_morts.Country.unique()
 continents_possibles = youth.Continent.unique()
 
+#style défaut
+StyleDropDown = {"width":"95%","height":"100%","display":"inline-block","margin-left":"50px","margin-right":"50px"}
 
-moy = youth.groupby('Continent').mean()
-moy
-fig3 = px.bar(moy , y = '15-19 years old, current drinkers both sexes (%)',
-                labels={'15-19 years old, current drinkers both sexes (%)':"consommation d'alcool<br> pour les 15-19 ans"},
-                template = "plotly_dark",
+# menu déroulant pour map
+def MenuDeroulantMap(id):
+    Menu = dcc.Dropdown(
+            id = id,
+            options = [{'label': str(i), 'value': str(i)} for i in continents_possibles if str(i)!="nan" and str(i)!="OC"],
+            value="EU"
             )
-fig3.update_layout(barmode='stack', xaxis={'categoryorder':'total descending'})
+    return Menu
+
+
 
 
 tab_total = dbc.Card(
@@ -44,17 +93,13 @@ tab_total = dbc.Card(
             dbc.Col([
                 dcc.Graph(
                     id='map',
-                    figure=fig
+                    figure=figTotal
                 ),
             ]),
         ]),
         dbc.Row([
-            dcc.Dropdown(
-                id = "pays_select",
-                options = [{'label': str(i), 'value': str(i)} for i in continents_possibles if str(i)!="nan" and str(i)!="OC"],
-                value="EU"
-            ),
-        ],style={"width":"95%","height":"100%","display":"inline-block","margin-left":"50px","margin-right":"50px"}),
+            MenuDeroulantMap("pays_select"),
+        ],style= StyleDropDown ),
         dbc.Row([
             dbc.Col([
                 dcc.Graph(
@@ -65,7 +110,7 @@ tab_total = dbc.Card(
         dbc.Row([
             dbc.Col([
                 dcc.Graph(
-                    id='histogramme', figure = fig3,
+                    id='histogramme', figure = figTotal3,
                     style = {'width': '100%','height': '100%',"background":"#000000"},
 
                 ),
@@ -75,9 +120,71 @@ tab_total = dbc.Card(
     ]), className ="mt-3", style = {'width': '100%','height': '100%',"background":"#131414"},
 )
 
-tab_homme = ""
+tab_homme = dbc.Card(
+    dbc.CardBody([
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='mapMan',
+                    figure=figTotalMan
+                ),
+            ]),
+        ]),
+        dbc.Row([
+            MenuDeroulantMap("pays_select2"),
+        ],style= StyleDropDown ),
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='mapMan2',
+                ),
+            ]),
+        ]),
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='histogrammeMan', figure = figTotalMan3,
+                    style = {'width': '100%','height': '100%',"background":"#000000"},
 
-tab_femme = ""
+                ),
+            ]),
+        ]),
+
+    ]), className ="mt-3", style = {'width': '100%','height': '100%',"background":"#131414"},
+)
+
+tab_femme = dbc.Card(
+    dbc.CardBody([
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='mapFemales',
+                    figure=figTotalFemales
+                ),
+            ]),
+        ]),
+        dbc.Row([
+            MenuDeroulantMap("pays_select3"),
+        ],style= StyleDropDown ),
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='mapFemales2',
+                ),
+            ]),
+        ]),
+        dbc.Row([
+            dbc.Col([
+                dcc.Graph(
+                    id='histogrammeFemales', figure = figTotalFemales3,
+                    style = {'width': '100%','height': '100%',"background":"#000000"},
+
+                ),
+            ]),
+        ]),
+
+    ]), className ="mt-3", style = {'width': '100%','height': '100%',"background":"#131414"},
+)
 
 # Layout de la page permettant d'afficher les differents éléments
 layout = html.Div([
@@ -94,36 +201,57 @@ layout = html.Div([
     ], style={'width': '100%','height': '100%',"background":"#131414","margin-top":"70px"})
 ]),
 
+#Interprétation menu déroulant
+def InterMenu(pays_selected):
+    pays = "europe"
+    name = "Europe"
+    if pays_selected == "SA":
+        pays = "south america"
+        name = "Amerique du sud"
+    elif pays_selected == "AS":
+        pays = "asia"
+        name = "Asie"
+    elif pays_selected == "AF":
+        pays = "africa"
+        name = "Afrique"
+    else :
+        pays = "europe"
+
+    return pays,name
+
 
 @app.callback(
     Output("map2","figure"),
     [Input("pays_select","value")]
 )
 def upgrade_map(pays_select):
-    pays = "europe"
-    name = "Europe"
-    if pays_select == "SA":
-        pays = "south america"
-        name = "Amerique du sud"
-    elif pays_select == "AS":
-        pays = "asia"
-        name = "Asie"
-    elif pays_select == "AF":
-        pays = "africa"
-        name = "Afrique"
-    else :
-        pays = "europe"
-
-    fig2 = px.choropleth(youth ,
-                               locations='Alpha-3 code',
-                               color='15-19 years old, current drinkers both sexes (%)',
-                               color_continuous_scale="Viridis",
-                               range_color=(0, 100),
-                               scope=pays,
-                               labels={'15-19 years old, current drinkers both sexes (%)':"consommation d'alcool<br> pour les 15-19 ans"},
-                               template="plotly_dark",
-                              )
+    pays,name = InterMenu(pays_select)
+    fig2 = choroplethGraph(pays,youth,youthColor,youthColorFrench,"Viridis")
     fig2.update_layout(title_text = "% de jeune consommant de l'alcool en "+ name),
+    fig2.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+
+    return fig2
+
+@app.callback(
+    Output("mapMan2","figure"),
+    [Input("pays_select2","value")]
+)
+def upgrade_map(pays_select):
+    pays,name = InterMenu(pays_select)
+    fig2 = choroplethGraph(pays,youth,youthColorMan,youthColorManFrench,"Viridis")
+    fig2.update_layout(title_text = "% de jeunes hommes consommant de l'alcool en "+ name),
+    fig2.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
+
+    return fig2
+
+@app.callback(
+    Output("mapFemales2","figure"),
+    [Input("pays_select3","value")]
+)
+def upgrade_map(pays_select):
+    pays,name = InterMenu(pays_select)
+    fig2 = choroplethGraph(pays,youth,youthColorFemales,youthColorFemalesFrench,"Viridis")
+    fig2.update_layout(title_text = "% de jeunes femmes consommant de l'alcool en "+ name),
     fig2.update_layout(margin={"r":0,"t":30,"l":0,"b":0})
 
     return fig2
